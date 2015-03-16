05cdb73ca3663edbac65160d28296b25e5dc4329
Wendy Leung <wendysarahleung@gmail.com>
2015-03-16 12:58:52 -0700

    test: change top 0 to bottom 300

--- hellotest.js
+++ hellotest.js
@@ -6,7 +6,7 @@
 
     /* add the css */
     myCSS  = '#my_unique_id, #my_unique_id *{font-family:Courier,"Courier New",sans-serif;color:#333;line-height:1.5em;font-size:15px;margin:0;padding:0;text-shadow:none;}';
-    myCSS += '#my_unique_id {z-index:10000;position:fixed;top:0;left:300px;background-color:#fff;}';
+    myCSS += '#my_unique_id {z-index:10000;position:fixed;bottom:300;left:0;background-color:#fff;}';
     myCSS += '#my_unique_id .c {width:200px; padding:20px;}';
     myCSS += '#my_unique_id h1 {font-size:20px; margin-bottom:0.5em;color:#0080C0}';
     myCSS += '#my_unique_id p {margin-bottom:0.5em;}';
