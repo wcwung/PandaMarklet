796084bd1c140246896b36d31407e284941020cc
Wendy Leung <wendysarahleung@gmail.com>
2015-03-16 14:09:51 -0700

    test: two words in value field

--- hellotest.js
+++ hellotest.js
@@ -70,7 +70,7 @@
 
     /* build the HTML element */
     myHTML  = '<div class="c">';
-    myHTML += 'Company: <input class="company" type="text" value=' + dataObj["Company"] + '>';
+    myHTML += 'Company: <input class="company" type="text" value="hello world">';
     myHTML += 'Job Title: <input class="jobtitle" type="text" value=' + dataObj["Job Title"] + '>';
     myHTML += 'Location: <input class="location" type="text" value=' + dataObj["Location"] + '>';
     myHTML += 'Experience: <input class="experience" type="text" value=' + dataObj["Experience"] + '>';
