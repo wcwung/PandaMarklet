b753b681e685f4205b22dd3d3e45e9d9c2b5984a
Wendy Leung <wendysarahleung@gmail.com>
2015-03-16 16:33:28 -0700

    Add AngelList with new updates

--- hellotest.js
+++ hellotest.js
@@ -1,11 +1,11 @@
 (function () {
-    var data = [];
-    var seperatedata =[];
-    var dataObj = {'Company': {}};
+    var jobdata = [];
+    var companydata =[];
+    var jobObject = {'Company': {}};
     var site = window.location.origin;
     var pathname = window.location.pathname;
 
-    var fillDataObj = function(obj, keys, values){
+    var fillJobObj = function(obj, keys, values){
       for( var i = 0; i < keys.length; i++ ){
         obj[keys[i]] = values[i];
       }
@@ -16,43 +16,48 @@
       var url = 'https://httpbin.org/post';
       http.open('POST', url, true);
       http.setRequestHeader('Content-type', 'application/json');
-      http.send( JSON.stringify(dataObj) );
+      http.send( JSON.stringify(jobObject) );
       alert('Job saved');
     };
 
     if( site + pathname.slice(0,6) === 'https://www.linkedin.com/jobs2'){
       var descriptions = ['Job Title', 'Location', 'Apply Link', 'Source Network'];
-      var seperate = ['Name', 'Day Posted', 'Experience', 'Job Function', 'Employment Type', 'Industry', 'Employer Job ID', 'Job ID'];
-      data.push( $('h1')[0].innerHTML );
-      data.push( $('div.content:eq(0)').find('span:eq(2)').text() );
-      data.push( $('#offsite-apply-button').attr('href') );
-      data.push( "LinkedIn" );
-
-      seperatedata.push( $('div.content:eq(0)').find('span:eq(0)').text() );
-      seperatedata.push( $('div.content:eq(0)').find('.posted').text() );
+      var company = ['Name', 'Day Posted', 'Experience', 'Job Function', 'Employment Type', 'Industry', 'Employer Job ID', 'Job ID'];
+      jobdata.push( $('h1')[0].innerHTML );
+      jobdata.push( $('div.content:eq(0)').find('span:eq(2)').text() );
+      jobdata.push( $('#offsite-apply-button').attr('href') );
+      jobdata.push( "LinkedIn" );
+      //companyObject 
+      companydata.push( $('div.content:eq(0)').find('span:eq(0)').text() );
+      companydata.push( $('div.content:eq(0)').find('.posted').text() );
       var otherDetails = $('.value');
       for( var i = 0; i < otherDetails.length; i++ ){
-        seperatedata.push(otherDetails[i].innerHTML)
+        companydata.push(otherDetails[i].innerHTML)
       }
 
-      fillDataObj(dataObj, descriptions, data);
-      fillDataObj(dataObj['Company'], seperate, seperatedata);
-      console.log(dataObj);
-    } 
-    // else if ( site + pathname.split('/')[2] === 'https://angel.cojobs' ){
-    //   var descriptions = ['Company', 'Job Title', 'Location', 'Job Function', 'Salary'];
-    //   var CompanyTitle = $('.join-title').text().split(' at ');
+      fillJobObj(jobObject, descriptions, jobdata);
+      fillJobObj(jobObject['Company'], company, companydata);
+      console.log(jobObject);
+    } else if ( site + pathname.split('/')[2] === 'https://angel.cojobs' ){
+      var descriptions = ['Job Title', 'Location', 'Apply Link', 'Source Network'];
+      var company = ['Name','Job Function', 'Salary'];
+      var CompanyTitle = $('.join-title').text().split(' at ');
 
-    //   data.push( CompanyTitle[1] );
-    //   data.push( CompanyTitle[0] );
-    //   data.push( $('.locations').text() );
-    //   data.push( $('.skills').text() );
-    //   data.push( $('.salary').text() );
+      jobdata.push( CompanyTitle[0] );
+      jobdata.push( $('.locations').text() );
+      jobdata.push( site + pathname );
+      jobdata.push( "AngelList" );
 
-    //   fillDataObj();
-    // }
+      jobdata.push( CompanyTitle[1] );
+      jobdata.push( $('.skills').text() );
+      jobdata.push( $('.salary').text() );
 
-    /* FORM DATA */
+      fillJobObj(jobObject, descriptions, jobdata);
+      fillJobObj(jobObject['Company'], company, companydata);
+      console.log(jobObject);
+    }
+
+    /* FORM */
     var myCSS, myStyleNode,
         myHTML, myHTMLNode;
 
@@ -69,45 +74,45 @@
 
     /* build the HTML element */
     myHTML  = '<div class="c">';
-    myHTML += 'Source Network: <input id="psource" type="text" value="' + (dataObj["Source Network"] || "") + '">';
-    myHTML += 'Company: <input id="pcompany" type="text" value="' + (dataObj["Company"]["Name"] || "") + '">';
-    myHTML += 'Job Title: <input id="pjobtitle" type="text" value="' + (dataObj["Job Title"] || "") + '">';
-    myHTML += 'Location: <input id="plocation" type="text" value="' + (dataObj["Location"] || "") + '">';
-    myHTML += 'Experience: <input id="pexperience" type="text" value="' + (dataObj["Company"]["Experience"] || "") + '">';
-    myHTML += 'Job Function/Skills: <input id="pjobfunction" type="text" value="' + (dataObj["Company"]["Job Function"] || "") + '">';
-    myHTML += 'Employment Type: <input id="pemployment" type="text" value="' + (dataObj["Company"]["Employment Type"] || "") + '">';
-    myHTML += 'Industry: <input id="pindustry" type="text" value="' + (dataObj["Company"]["Industry"] || "") + '">';
+    myHTML += 'Source Network: <input id="psource" type="text" value="' + (jobObject["Source Network"] || "") + '">';
+    myHTML += 'Company: <input id="pcompany" type="text" value="' + (jobObject["Company"]["Name"] || "") + '">';
+    myHTML += 'Job Title: <input id="pjobtitle" type="text" value="' + (jobObject["Job Title"] || "") + '">';
+    myHTML += 'Location: <input id="plocation" type="text" value="' + (jobObject["Location"] || "") + '">';
+    myHTML += 'Experience: <input id="pexperience" type="text" value="' + (jobObject["Company"]["Experience"] || "") + '">';
+    myHTML += 'Job Function/Skills: <input id="pjobfunction" type="text" value="' + (jobObject["Company"]["Job Function"] || "") + '">';
+    myHTML += 'Employment Type: <input id="pemployment" type="text" value="' + (jobObject["Company"]["Employment Type"] || "") + '">';
+    myHTML += 'Industry: <input id="pindustry" type="text" value="' + (jobObject["Company"]["Industry"] || "") + '">';
     myHTML += '<button id="pandabutton">Submit</button>';
     myHTML += '</div>';
     /* and create the node */
     myHTMLNode = document.createElement('div');
     myHTMLNode.id = 'my_unique_id';
     myHTMLNode.innerHTML = myHTML;
-
-    /* add js functionality to it */
-    var updataObj = function(){
-      dataObj['Origin URL'] = site;
-      dataObj['Job URL'] = pathname;
-      dataObj['Source Network'] = document.getElementById('psource').value;    
-      dataObj['Job Title'] = document.getElementById('pjobtitle').value;
-      dataObj['Location'] = document.getElementById('plocation').value;
-      dataObj['Company']['Name'] = document.getElementById('pcompany').value;
-      dataObj["Company"]['Experience'] = document.getElementById('pexperience').value;
-      dataObj["Company"]['Job Function'] = document.getElementById('pjobfunction').value;
-      dataObj["Company"]['Employment Type'] = document.getElementById('pemployment').value;
-      dataObj["Company"]['Industry'] = document.getElementById('pindustry').value;
-
-      console.log(dataObj);
-      sendData();
-    };
     /* injec the node, with the event attached */
     document.body.appendChild(myHTMLNode);
 
+    /* listener/ updata jobObject / POST jobObject */
+    var updataJobObj = function(){
+      jobObject['Origin URL'] = site;
+      jobObject['Job URL'] = pathname;
+      jobObject['Source Network'] = document.getElementById('psource').value;    
+      jobObject['Job Title'] = document.getElementById('pjobtitle').value;
+      jobObject['Location'] = document.getElementById('plocation').value;
+      jobObject['Company']['Name'] = document.getElementById('pcompany').value;
+      jobObject["Company"]['Experience'] = document.getElementById('pexperience').value;
+      jobObject["Company"]['Job Function'] = document.getElementById('pjobfunction').value;
+      jobObject["Company"]['Employment Type'] = document.getElementById('pemployment').value;
+      jobObject["Company"]['Industry'] = document.getElementById('pindustry').value;
+
+      console.log(jobObject);
+      sendData();
+    };
+
     var pandabutton = document.getElementById('pandabutton');
     if (pandabutton.addEventListener) {
-      pandabutton.addEventListener('click', updataObj, false);
+      pandabutton.addEventListener('click', updataJobObj, false);
     } else if (el.attachEvent) {
-      pandabutton.attachEvent('onclick', updataObj);
+      pandabutton.attachEvent('onclick', updataJobObj);
     }
     
   
